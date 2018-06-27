43976c868d0d12e7c4e2a1a80869b6f74b1136c9
Geraint Edwards <gedgey@gmail.com>
2018-06-27 09:32:40 +0100

    revert expected status from dataset API for adding event
    it should be StatusCreated, and will be Real Soon Now[tm]

--- client/dataset_api_client_test.go
+++ client/dataset_api_client_test.go
@@ -266,13 +266,13 @@ func TestDatasetAPIClientAddEventToInstanceUnexpectedStatus(t *testing.T) {
 	Convey("Given datasetAPIClient has been configured correctly", t, func() {
 		_, _, httpClient, cli := setup(nil, http.StatusBadRequest)
 
-		Convey("When the returned HTTP status is not 200 OK", func() {
+		Convey("When the returned HTTP status is not 201 CREATED", func() {
 
 			err := cli.AddEventToInstance(testInstanceID, event)
 
 			Convey("Then the expected error is returned", func() {
 				url := fmt.Sprintf(addInstanceEventURL, host, testInstanceID)
-				So(err.Error(), ShouldEqual, incorrectStatusError("AddEventToInstance", url, http.MethodPost, http.StatusOK, http.StatusBadRequest).Error())
+				So(err.Error(), ShouldEqual, incorrectStatusError("AddEventToInstance", url, http.MethodPost, http.StatusCreated, http.StatusBadRequest).Error())
 			})
 
 			Convey("And httpClient.Do is called 1 time", func() {
@@ -287,7 +287,7 @@ func TestDatasetAPIClientAddEventToInstanceUnexpectedStatus(t *testing.T) {
 
 func TestDatasetAPIClientAddEventToInstance(t *testing.T) {
 	Convey("Given datasetAPIClient has been configured correctly", t, func() {
-		_, _, httpClient, cli := setup(nil, http.StatusOK)
+		_, _, httpClient, cli := setup(nil, http.StatusCreated)
 
 		Convey("When AddEventToInstance is called", func() {
 			err := cli.AddEventToInstance(testInstanceID, event)
